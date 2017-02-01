a154edd65b6b2d50f4e6d0d7f4810ef28e2cb08e
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-02-01 12:38:56 -0600

    Enemy Skeleton Made

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    [SerializeField]
+    private Attribute m_health = new Attribute();
+    [SerializeField]
+    private Attribute m_attack = new Attribute();
+    [SerializeField]
+    private Attribute m_defense = new Attribute();
+
+    public Attribute health { get {return m_health;} }
+    public Attribute attack { get { return m_attack; } }
+    public Attribute defense { get { return m_defense; } }
+
+    //TODO: Set a way for the enemy to listen for the Events that are declared in the CombatManager
+
+    private void OnCombatBegin()
+    {
+        
+    }
+
+    private void OnCombatUpdate()
+    {
+
+    }
+
+    private void OnCombatEnd()
+    {
+
+    }
+
+    private void OnPlayerTurn()
+    {
+
+    }
+}
