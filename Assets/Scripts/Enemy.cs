3951b2256fa16430c888370a59c15f0c42a3023e
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-02-01 12:48:33 -0600

    Consoltation with Ben

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class Enemy
 {
-    [SerializeField]
-    private Attribute m_health = new Attribute();
-    [SerializeField]
-    private Attribute m_attack = new Attribute();
-    [SerializeField]
-    private Attribute m_defense = new Attribute();
-
-    public Attribute health { get {return m_health;} }
-    public Attribute attack { get { return m_attack; } }
-    public Attribute defense { get { return m_defense; } }
+    public Attribute health = new Attribute();
+    public Attribute attack = new Attribute();
+    public Attribute defense = new Attribute();
 
     //TODO: Set a way for the enemy to listen for the Events that are declared 
         //TODO: in the CombatManager
