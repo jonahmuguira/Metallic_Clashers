6b4840679aed228024512f49e7a3e481d0397569
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-02-07 11:03:36 -0600

    Nodes can be saved
    THe Nodes can be Saved and Loaded to and from a file.

--- Assets/Scripts/NodeManager.cs
+++ Assets/Scripts/NodeManager.cs
@@ -1,29 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Serialization;
 
 using UnityEngine;
 
 using Library;
 
-public class NodeManager : MonoSingleton<NodeManager>
+[Serializable]
+public class NodeManager : MonoBehaviour
 {
-    public int level;
+    [XmlIgnore]
+    public int level;   //Testing Purposes only
     public List<Node> nodes = new List<Node>();
 
-    protected override void Awake()
+    public NodeManager() { }
+
+    protected void Awake()
     {
-        base.Awake();
         nodes = FindObjectsOfType<MonoNode>().Select(node => node.node).ToList();
         foreach (var n in nodes)
         {
             if(n.unlockedAtLevel <= level && n.nodeState == Node.NodeState.Locked)
                 n.nodeState = Node.NodeState.Unlocked;
         }
-        nodes[nodes.Count - 1].nodeState = Node.NodeState.Completed;
+        nodes.Last().nodeState = Node.NodeState.Completed;
+    }
+
+    [ContextMenu("Save Node System")]
+    private void Save()
+    {
+        var nodePath = Environment.CurrentDirectory + "/NodeSystemData.xml";
+        var nodeStream = File.Create(nodePath);
+
+        var serializer = new XmlSerializer(typeof(List<Node>));
+        serializer.Serialize(nodeStream, nodes);
+        nodeStream.Close();
+    }
+
+    [ContextMenu("Load Node System")]
+    private List<Node> Load()
+    {
+        var reader = new XmlSerializer(typeof(List<Node>));
+        var file = new StreamReader(Environment.CurrentDirectory + "/NodeSystemData.xml");
+
+        var oldNodeManager = (List<Node>)reader.Deserialize(file);
+        file.Close();
+        return oldNodeManager;
     }
 }
 
+[Serializable]
 public class Node
 {
     public enum NodeState
