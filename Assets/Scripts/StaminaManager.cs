14ff29f2d4ec46ac27ef02794c46ebc00091be6a
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-02-15 12:43:20 -0600

    Stamina Started

---
+++ Assets/Scripts/StaminaManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+using Library;
+
+using UnityEngine;
+
+public class StaminaInformation
+{
+    public int value;
+    public string timeLastPlayed;
+}
+
+public class StaminaManager : MonoSingleton<StaminaManager>
+{     
+    public int value;
+    public float staminaRate;
+
+    [SerializeField]
+    private DateTime m_TimeLastPlayed = DateTime.Now;
+    private float m_LastFrameTime;
+
+	public void Start()
+	{
+	    var playerDateLastPlayer = GameManager.self.playerData.dateLastPlayed;
+	    if (playerDateLastPlayer == null)
+	    {
+            playerDateLastPlayer = new StaminaInformation
+            {
+                timeLastPlayed = DateTime.Now.ToString("u")
+            };
+	        return;
+	    }
+
+        m_TimeLastPlayed = DateTime.Parse(playerDateLastPlayer.timeLastPlayed);
+
+	    var yearChange = DateTime.Now.Year - m_TimeLastPlayed.Year;
+	}	
+
+	private IEnumerable UpdateStamina()
+	{
+	    var currentTime = Time.time%staminaRate;
+        if (currentTime < m_LastFrameTime)
+        {
+            value++;
+        }
+        m_LastFrameTime = currentTime;
+	    yield return null;
+	}
+}
