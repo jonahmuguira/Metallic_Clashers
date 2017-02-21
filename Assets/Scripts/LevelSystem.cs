c753898e4b0c9d408c4d11b25dcd30804268b959
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-02-21 10:56:25 -0600

    Level System Return Error Update Commit
    Fixed the return value error within the Calculate Level function. Just
    need to setup the other LevelInfo variable values.

--- Assets/Scripts/LevelSystem.cs
+++ Assets/Scripts/LevelSystem.cs
@@ -15,27 +15,34 @@ public class LevelSystem
 
     public LevelInfo playerLevelInfo;
 
-    //private LevelInfo CalculateLevel(uint exp)
-    //{
-    //    //base_xp * (level_to_get ^ factor);
+    private LevelInfo CalculateLevel(uint exp)
+    {
+        //base_xp * (level_to_get ^ factor);
 
-    //    //base_xp = constant, how much xp needed to levelup.
-    //    //level_to_get = level aiming for.
-    //    //factor = constant, how much of an increase of xp needed for levelup.
+        //base_xp = constant, how much xp needed to levelup.
+        //level_to_get = level aiming for.
+        //factor = constant, how much of an increase of xp needed for levelup.
 
-    //    var tempExperience = exp;
+        var tempExperience = exp;
 
-    //    const uint c_baseExperience = 100;
-    //    uint level = 1;
-    //    const uint c_factor = 2;
+        const uint c_baseExperience = 100;
+        uint tempLevel = 1;
+        const uint c_factor = 2;
 
-    //    var mathFormula = c_baseExperience * ((level + 1) ^ c_factor);
+        var mathFormula = c_baseExperience * ((tempLevel + 1) ^ c_factor);
 
-    //    for (level = 1; tempExperience > mathFormula; level++)
-    //    {
-    //        tempExperience -= mathFormula;
-    //    }
-    //}
+        for (tempLevel = 1; tempExperience > mathFormula; tempLevel++)
+        {
+            tempExperience -= mathFormula;
+        }
+
+        var levelInfo = new LevelInfo()
+        {
+            level = tempLevel,
+        };
+
+        return levelInfo;
+    }
 
     public void IsLeveledUp(uint currentExperience, uint modifier)
     {
