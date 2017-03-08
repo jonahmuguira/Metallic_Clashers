92febe517b08dd831385dd467270301b41901886
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-03-08 12:42:03 -0600

    Dropping Enemies in scene
    Will not activate the Enemies dropping yep. I have the code stored
    seperately. We will put it in when Combat Manager is ready

---
+++ Assets/Scripts/EnemyMono.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class EnemyMono : MonoBehaviour
+{
+    public Enemy enemy;
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using System.Xml.Serialization;
-
-using Board;
 
 using Library;
 
