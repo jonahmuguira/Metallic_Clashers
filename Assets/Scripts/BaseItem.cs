6b33a3ab36f3452b2b3c95d13ee0ee762e838ce6
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-03-08 12:36:52 -0600

    Base Item Script and UML Commit
    Created a base item script and uml document. Both the script and uml are
    currently in progress at the moment.

---
+++ Assets/Scripts/BaseItem.cs
@@ -0,0 +1,43 @@
+﻿/*
+ Script Info
+ Script Name: BaseItem.cs
+ Created by: Brock Barlow
+ This script is used to handle items.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class BaseItem
+{
+    private int m_Inventory; //item container.
+
+    private int m_Turns; //will determine how long effect will last based on turns.
+    private bool m_IsTurnActive = false; //starts false, becomes true when active.
+
+    private float m_Timer; //will determine how long effect will last based on time.
+    private bool m_IsTimeActive = false; //starts false, becomes true when active.
+
+    public uint modifier; //value that will "modifier" player attribute(s).
+}
+
+[Serializable]
+public class InstantBased : BaseItem
+{
+    
+}
+
+[Serializable]
+public class TurnBased : BaseItem
+{
+    
+}
+
+[Serializable]
+public class TimeBased : BaseItem
+{
+
+}
