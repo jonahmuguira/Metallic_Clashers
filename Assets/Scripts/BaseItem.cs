a0ffbd2d0c75b7faf865f5fd35f508bd0c4f2145
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-03-13 11:47:22 -0500

    BaseItem Script and Documentation Update Commit
    Updated base item script and documentation. Also added ui mockup from
    morning meeting on 3/13/2017.

--- Assets/Scripts/BaseItem.cs
+++ Assets/Scripts/BaseItem.cs
@@ -13,15 +13,12 @@ using UnityEngine;
 [Serializable]
 public class BaseItem
 {
-    private int m_Inventory; //item container.
-
-    private int m_Turns; //will determine how long effect will last based on turns.
-    private bool m_IsTurnActive = false; //starts false, becomes true when active.
-
-    private float m_Timer; //will determine how long effect will last based on time.
-    private bool m_IsTimeActive = false; //starts false, becomes true when active.
-
     public uint modifier; //value that will "modifier" player attribute(s).
+
+    protected virtual void UseItem() //use item function. TODO - define what function needs and return type
+    {
+        
+    }
 }
 
 [Serializable]
@@ -33,11 +30,13 @@ public class InstantBased : BaseItem
 [Serializable]
 public class TurnBased : BaseItem
 {
-    
+    private int m_Turns; //will determine how long effect will last based on turns.
+    private bool m_IsTurnActive = false; //starts false, becomes true when active.
 }
 
 [Serializable]
 public class TimeBased : BaseItem
 {
-
+    private float m_Timer; //will determine how long effect will last based on time.
+    private bool m_IsTimeActive = false; //starts false, becomes true when active.
 }
