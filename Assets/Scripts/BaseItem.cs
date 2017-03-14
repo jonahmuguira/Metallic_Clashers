1651ff0b58a57435606e054acd034cadeff07aee
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-03-14 10:52:30 -0500

    BaseItem UML and Script Update Commit
    Updated uml to version 14, added comments to script.

--- Assets/Scripts/BaseItem.cs
+++ Assets/Scripts/BaseItem.cs
@@ -15,62 +15,98 @@ public class BaseItem
 {
     private float m_age; //current durabilities max value.
 
-    public float durability = 0; //how long the item will last.
+    public float durability; //how long the item will last.
     public uint modifier; //value that will "modify" player attribute(s).
 
-    public virtual void UseItem() { }
+    public virtual void UseItem()
+    {
+        
+    }
 }
 
-//[Serializable]
+[Serializable]
+public class InstantItem : BaseItem 
+{
+    //instant item idea: heal up item.
+    //will "restore" the player's missing health value, but not fully.
+    //effect will be instant, no lasting/timed effect.
+
+    //instant item idea: instant attack item.
+    //will attack the enemy for a set amount of damage.
+    //effect will be instant, no lasting/timed effect.
+
+    //no attributes
+
+    public override void UseItem()
+    {
+        
+    }
+}
+
+[Serializable]
 public class TurnBased : BaseItem
 {
+    //these items are based on the amount of turns the player makes during play.
+
+    //no attributes
+
     public void UpdateSelf()
     {
         
     }
 }
 
-//[Serializable]
+[Serializable]
+public class TurnItem : TurnBased
+{
+    //turn item idea: attack increase item.
+    //will raise the player's attack value.
+    //effect will have a lasting/timed effect (based off turns).
+
+    //turn item idea: defense increase item.
+    //will raise the player's defense value.
+    //effect will have a lasting/timed effect (based off turns).
+
+    //no attributes
+
+    public override void UseItem()
+    {
+
+    }
+
+    //public void test()
+    //{
+    //    var test = durability + 1;
+    //}
+}
+
+[Serializable]
 public class TimeBased : BaseItem
 {
-    public void UpdateSelf()
+    //these items are based on a timer when activated by player during play.
+
+    //no attributes
+
+    public void UpdateSelf(float value)
     {
 
     }
 }
 
-//[Serializable]
-public class InstantItemTestClass : BaseItem
+[Serializable]
+public class TimeItem : TimeBased
 {
+    //time item idea: attack increase item.
+    //will raise the player's attack value.
+    //effect will have a lasting/timed effect (based off time).
+
+    //time item idea: defense increase item.
+    //will raise the player's defense value.
+    //effect will have a lasting/timed effect (based off time).
+
+    //no attributes
+
     public override void UseItem()
-    {
-        //Instantly do effect. No lasting effects.
-    }
-}
-
-//[Serializable]
-public class TurnItemTestClass : TurnBased
-{
-    public override void UseItem()
-    {
-        
-    }
-
-    public void test()
-    {
-        var test = durability + 1;
-    }
-}
-
-//[Serializable]
-public class TimeItemTestClass : TimeBased
-{
-    public override void UseItem()
-    {
-
-    }
-
-    public void test()
     {
 
     }
