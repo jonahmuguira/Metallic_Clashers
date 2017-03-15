d1572119cea2e798a50b4030df3faa740429e14f
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-03-15 16:15:54 -0500

    Move Enemy Scripts to Combat Namespace

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -1,113 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
-
-using Combat;
 using Combat.Board;
-
 using UnityEngine;
 
-[Serializable]
-public class Enemy
+namespace Combat
 {
-    public Attribute health = new Attribute();
-    public Attribute attack = new Attribute();
-    public Attribute defense = new Attribute();
-
-    public float attackSpeed;
-    public int movesUntilAttack;
-    public GemType damageType;
-
-    public List<GemType> resistances;
-    public List<GemType> weaknesses;
-
-    private int movesCounter = 0;
-    private float attackCountdown;
-
-    public Enemy()
+    [Serializable]
+    public class Enemy
     {
-        health.value = 10;
-        health.coefficient = 1;
+        public Attribute health = new Attribute();
+        public Attribute attack = new Attribute();
+        public Attribute defense = new Attribute();
 
-        attack.value = 10;
-        attack.coefficient = 1;
+        public float attackSpeed;
+        public int movesUntilAttack;
+        public GemType damageType;
 
-        defense.value = 10;
-        defense.coefficient = 1;
+        public List<GemType> resistances;
+        public List<GemType> weaknesses;
 
-        attackSpeed = 5;
-        attackCountdown = attackSpeed;
+        private int movesCounter = 0;
+        private float attackCountdown;
 
-        movesUntilAttack = 3;
-    }
-
-    public Enemy(float pHealth, float pAttack, float pDefense, float pattackSpeed, int pmovesUntilAttack)
-    {
-        health.value = pHealth;
-        health.coefficient = 1;
-
-        attack.value = pAttack;
-        attack.coefficient = 1;
-
-        defense.value = pDefense;
-        defense.coefficient = 1;
-
-        attackSpeed = pattackSpeed;
-        attackCountdown = attackSpeed;
-
-        movesUntilAttack = pmovesUntilAttack;
-
-    }
-
-    public void OnCombatBegin()
-    {
-        CombatManager.self.onPlayerTurn.AddListener(OnPlayerTurn);
-        CombatManager.self.onCombatEnd.AddListener(OnCombatEnd);
-        CombatManager.self.onCombatUpdate.AddListener(OnCombatUpdate);
-    }
-
-    private void OnCombatUpdate()
-    {
-        attackCountdown -= Time.deltaTime;
-
-        if (attackCountdown > 0) return;
-        Attack();
-        attackCountdown = attackSpeed;
-    }
-
-    private void OnCombatEnd()
-    {
-        //TODO: Define what the enemy should do here. Possibly Nothing.
-    }
-
-    private void OnPlayerTurn()
-    {
-        movesCounter++;
-        if (movesCounter != 0 && movesCounter % movesUntilAttack == 0)
+        public Enemy()
         {
+            health.value = 10;
+            health.coefficient = 1;
+
+            attack.value = 10;
+            attack.coefficient = 1;
+
+            defense.value = 10;
+            defense.coefficient = 1;
+
+            attackSpeed = 5;
+            attackCountdown = attackSpeed;
+
+            movesUntilAttack = 3;
+        }
+
+        public Enemy(float pHealth, float pAttack, float pDefense, float pattackSpeed, int pmovesUntilAttack)
+        {
+            health.value = pHealth;
+            health.coefficient = 1;
+
+            attack.value = pAttack;
+            attack.coefficient = 1;
+
+            defense.value = pDefense;
+            defense.coefficient = 1;
+
+            attackSpeed = pattackSpeed;
+            attackCountdown = attackSpeed;
+
+            movesUntilAttack = pmovesUntilAttack;
+
+        }
+
+        public void OnCombatBegin()
+        {
+            CombatManager.self.onPlayerTurn.AddListener(OnPlayerTurn);
+            CombatManager.self.onCombatEnd.AddListener(OnCombatEnd);
+            CombatManager.self.onCombatUpdate.AddListener(OnCombatUpdate);
+        }
+
+        private void OnCombatUpdate()
+        {
+            attackCountdown -= Time.deltaTime;
+
+            if (attackCountdown > 0) return;
             Attack();
+            attackCountdown = attackSpeed;
         }
-    }
 
-    private void Attack()
-    {
-        GameManager.self.playerData.TakeDamage(attack.totalValue, damageType);
-    }
-
-    public void TakeDamage(float damage, GemType gemType)
-    {
-        var percentage = damage / defense.totalValue;
-        var finalDamage = damage * Mathf.Clamp(percentage, 0f, 1f);
-
-        if (resistances.Contains(gemType))
+        private void OnCombatEnd()
         {
-            finalDamage *= .75f;
+            //TODO: Define what the enemy should do here. Possibly Nothing.
         }
 
-        else if (weaknesses.Contains(gemType))
+        private void OnPlayerTurn()
         {
-            finalDamage *= 1.25f;
+            movesCounter++;
+            if (movesCounter != 0 && movesCounter % movesUntilAttack == 0)
+            {
+                Attack();
+            }
         }
 
-        health.modifier -= finalDamage;
+        private void Attack()
+        {
+            GameManager.self.playerData.TakeDamage(attack.totalValue, damageType);
+        }
+
+        public void TakeDamage(float damage, GemType gemType)
+        {
+            var percentage = damage / defense.totalValue;
+            var finalDamage = damage * Mathf.Clamp(percentage, 0f, 1f);
+
+            if (resistances.Contains(gemType))
+            {
+                finalDamage *= .75f;
+            }
+
+            else if (weaknesses.Contains(gemType))
+            {
+                finalDamage *= 1.25f;
+            }
+
+            health.modifier -= finalDamage;
+        }
     }
 }
