3afc585257e6ba032f6dad39356f5f42410596b7
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-03-21 17:31:37 -0500

    Item Management Commit
    Finalized base item script. Created item manager uml. Created items
    folder. Updated base item uml.

---
+++ Assets/Scripts/Items/ItemManager.cs
@@ -0,0 +1,9 @@
+﻿/* Script Info - Script Name: ItemManager.cs, Created by: Brock Barlow, This script is used to handle items. */
+
+namespace Items
+{
+    public class ItemManager
+    {
+    
+    }
+}
