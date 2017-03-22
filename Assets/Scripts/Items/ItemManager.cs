aafd35bc1b31230f0202f108f973f8276d4fd6a3
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-03-22 12:50:12 -0500

    Item Manager Script Edits Commit
    made edits to the item manager script. made small edits to the base item
    script.

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -2,8 +2,36 @@
 
 namespace Items
 {
+    using System.Collections.Generic;
+
     public class ItemManager
     {
-    
+        private List<BaseItem> m_ActiveList; //list of item(s) being used
+        public List<BaseItem> inventory;     //list of item(s)
+        public BaseItem item;
+
+        public List<BaseItem> activeList { get { return m_ActiveList; } }
+
+        public void Update()
+        {
+            activeList.RemoveAll(i => i.Alive == false);
+        }
+
+        public void SetItemActive()
+        {
+            //1) Check if the item is in the inventory.
+                // put the item on the active list.
+                // remove the item from the inventory list.
+            //2) Use the item.
+            //3) Determine the item type (attack or defense?)
+            //4) Add listeners.
+
+            if (inventory.Contains(item)) { activeList.Add(item); inventory.Remove(item); } else { return; }
+
+            item.UseItem();
+
+            //currently in progress...
+            item.GetType();
+        }
     }
 }
