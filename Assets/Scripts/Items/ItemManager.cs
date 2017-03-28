c2320cfb30b53c40021d4c0a6ead632a0ac90dce
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-03-28 17:48:55 -0500

    My test scene and small script edits commit
    Updated my test scene and made minor edits to item manager.

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -10,54 +10,28 @@ namespace Items
         private List<BaseItem> m_ActiveList; //list of item(s) being used
         public List<BaseItem> activeList { get { return m_ActiveList; } }
         public List<BaseItem> inventory;     //list of item(s)
-        public int sizeOfList;               //used to find size of list(s)
         public TurnBased item;
+
+        public void SetCapacity() { inventory.Capacity = 15; activeList.Capacity = 6; }
+        //sets the capacity of item lists. //15 and 6 are test values that will get changed later.
 
         public void Update() { activeList.RemoveAll(i => i.Alive == false); }
         //needs to be called after using an item.
 
-        public void AddToInventory() { inventory.Add(item); }
-        //simply adds the item(s) to the inventory list.
-
         public void SetItemActive()
         {
-            //3) Determine the item type (attack or defense?)
-            //4) Add listeners.
-
             if (inventory.Contains(item)) { activeList.Add(item); inventory.Remove(item); } else { return; }
 
             item.UseItem();
 
             var itemType = item.GetType();
 
-            if (itemType == typeof(InstantItem))
-            {
-                item.UseItem();
-                Update();
-            }
-            else if (itemType == typeof(TurnBased))
-            {
-                CombatManager.self.onPlayerTurn.AddListener(item.UpdateSelf);
-                item.UseItem();
-                Update();
-            }
-            else if (itemType == typeof(TimeBased))
-            {
-                CombatManager.self.onCombatUpdate.AddListener(item.UpdateSelf);
-                item.UseItem();
-                Update();
-            }
+            if (itemType == typeof(InstantItem)) { Update(); }
+            else if (itemType == typeof(TurnBased)) { CombatManager.self.onPlayerTurn.AddListener(item.UpdateSelf); Update(); }
+            else if (itemType == typeof(TimeBased)) { CombatManager.self.onCombatUpdate.AddListener(item.UpdateSelf); Update(); }
         }
 
         public void SetItemNonActive() { if (activeList.Contains(item)) { inventory.Add(item); activeList.Remove(item); } }
         //if active list has any item(s) still after battle, remove them and add them back to the inventory list.
     }
 }
-
-
-//need to know if inventory has limit (yes, but how many?).
-//need to know if active inventory has limit (yes, but how many?).
-//
-//we have two item slots for active: one for heal and one for other.
-//need to limit # of heals and others.
-//slot 1 only has heal, slot 2 only has turn/time buff.
