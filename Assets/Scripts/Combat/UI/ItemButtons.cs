6a0ae926e11c15cc66bdabadc86c76a4eb5cf93d
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-04-11 17:53:00 -0500

    Item Work
    Updated my scene, made some changes to the item manager and game
    manager, created a item buttons script for the item ui buttons.

---
+++ Assets/Scripts/Combat/UI/ItemButtons.cs
@@ -0,0 +1,28 @@
+﻿namespace Combat.UI
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class ItemButtons : MonoBehaviour
+    {
+        [SerializeField]
+        private Button m_Button;
+        [SerializeField]
+        private Image m_Image;
+        [SerializeField]
+        private Text m_ModeText;
+
+        private void Awake()
+        {
+            
+        }
+
+        private static void OnClick()
+        {
+            
+        }
+    }
+}
