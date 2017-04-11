601568ce1364a3a360319490a2cecb15e5a7bda4
Benjamin Odom <bennybroseph@gmail.com>
2017-04-11 06:23:41 -0500

    Poor Drawing, but Drawing
    Draws Input Recordings

--- Assets/Scripts/InputReplay.cs
+++ Assets/Scripts/InputReplay.cs
@@ -18,16 +18,18 @@ public class InputReplay : MonoSingleton<InputReplay>
 {
     [SerializeField]
     private bool m_Replay;
+    [SerializeField]
+    private bool m_DrawData;
 
     private InputData m_InputData;
 
     protected override void OnAwake()
     {
-        if (!m_Replay)
-            return;
-
         var jsonData = File.ReadAllText(Application.persistentDataPath + "/InputData.json");
         m_InputData = JsonUtility.FromJson<InputData>(jsonData);
+
+        if (!m_Replay)
+            return;
 
         Random.InitState(m_InputData.randomSeed);
     }
@@ -54,6 +56,49 @@ public class InputReplay : MonoSingleton<InputReplay>
             ProcessDrag(currentDragAction);
             m_InputData.dragActions.Remove(currentDragAction);
         }
+    }
+
+    private void OnGUI()
+    {
+        if (m_DrawData)
+            DrawData();
+    }
+
+    private void DrawData()
+    {
+        CreateLineMaterial();
+        // Apply the line material
+        lineMaterial.SetPass(0);
+
+        GL.PushMatrix();
+        {
+            GL.MultMatrix(transform.localToWorldMatrix);
+
+            GL.Begin(GL.QUADS);
+            {
+                foreach (var dragAction in m_InputData.dragActions.Where(action => action.inputType == InputType.End))
+                {
+                    var origin = dragAction.dragInformation.origin;
+                    origin.y = Screen.height - origin.y;
+
+                    var end = dragAction.dragInformation.end;
+                    end.y = Screen.height - end.y;
+
+                    var direction = new Vector2(end.x - origin.x, end.y - origin.y).normalized;
+
+                    GL.Color(Color.green);
+                    GL.Vertex3(origin.x, origin.y, 0f);
+                    GL.Vertex3(origin.x + 15f * direction.y, origin.y + 15f * -direction.x, 0f);
+
+                    GL.Color(Color.red);
+                    GL.Vertex3(end.x + 15f * direction.y, end.y + 15f * -direction.x, 0f);
+                    GL.Vertex3(end.x, end.y, 0f);
+                    
+                }
+            }
+            GL.End();
+        }
+        GL.PopMatrix();
     }
 
     private static void ProcessTouch(TouchAction touchAction)
@@ -113,6 +158,26 @@ public class InputReplay : MonoSingleton<InputReplay>
         {
             if (button != null)
                 button.onClick.Invoke();
+        }
+    }
+
+    static Material lineMaterial;
+    static void CreateLineMaterial()
+    {
+        if (!lineMaterial)
+        {
+            // Unity has a built-in shader that is useful for drawing
+            // simple colored things.
+            Shader shader = Shader.Find("Hidden/Internal-Colored");
+            lineMaterial = new Material(shader);
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+            // Turn on alpha blending
+            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            // Turn backface culling off
+            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+            // Turn off depth writes
+            lineMaterial.SetInt("_ZWrite", 0);
         }
     }
 }
