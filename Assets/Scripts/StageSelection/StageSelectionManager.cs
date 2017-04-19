cb5da11c67b76e65de0b6bb1981b18b7e2c95e7d
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2017-04-19 14:52:17 -0500

    Stage Selection Panel
    Adding Assets from the Artists concerning the StageSelection panel.

--- Assets/Scripts/StageSelection/StageSelectionManager.cs
+++ Assets/Scripts/StageSelection/StageSelectionManager.cs
@@ -32,6 +32,8 @@ namespace StageSelection
         [SerializeField]
         private Text m_EnemyText;
         [SerializeField]
+        private Text m_StaminaCostText;
+        [SerializeField]
         private Button m_StartComabtButton;
         [SerializeField]
         private RectTransform m_NodeAnchor;
@@ -284,6 +286,7 @@ namespace StageSelection
             //Set Up UI
             m_StageNameText.text = "";
             m_EnemyText.text = "";
+            m_StaminaCostText.text = "";
             m_StartComabtButton.onClick.AddListener(OnStageSelectionEnd);
             m_StartComabtButton.interactable = false;
         }
@@ -403,6 +406,7 @@ namespace StageSelection
         private void DisplayInformation()
         {
             m_StageNameText.text = "Stage: " + m_CurrentNode.stageName;
+            m_StaminaCostText.text = "Stamina Cost: " + m_CurrentNode.staminaCost;
 
             // Enemy Display - Text
             m_EnemyText.text = "";
@@ -418,7 +422,7 @@ namespace StageSelection
 
             foreach (var key in enemyInstances)
             {
-                m_EnemyText.text += key.Key + " x" + key.Value;
+                m_EnemyText.text += key.Key + " - x" + key.Value;
             }
 
             // Not Enough Stamina clause
@@ -432,7 +436,7 @@ namespace StageSelection
                 || m_CurrentNode.prevNodes.Count == 0)
             {
                 m_StartComabtButton.interactable = true;
-                m_StartComabtButton.gameObject.GetComponentInChildren<Text>().text = "Engage";
+                m_StartComabtButton.gameObject.GetComponentInChildren<Text>().text = " Engage!";
             }
             else
             {
--- Assets/Scripts/StageSelection/StaminaDisplay.cs
+++ Assets/Scripts/StageSelection/StaminaDisplay.cs
@@ -1,22 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Text))]
-public class StaminaDisplay : MonoBehaviour
+namespace StageSelection
 {
-    private Text m_Text;
+    public class StaminaDisplay : MonoBehaviour
+    {
+        private Text m_Text;
+        [SerializeField]
+        private Image m_FillImage;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    m_Text = gameObject.GetComponent<Text>();
-	}
+        // Use this for initialization
+        void Start ()
+        {
+            
+            m_Text = transform.GetComponentInChildren<Text>();
+        }
 	
-	// Update is called once per frame
-	void Update ()
-	{
-	    m_Text.text = StaminaManager.self.value + "/" + StaminaManager.self.maxValue;
-	}
+        // Update is called once per frame
+        void Update ()
+        {
+            m_FillImage.fillAmount = (float)StaminaManager.self.value / StaminaManager.self.maxValue;
+            m_Text.text = StaminaManager.self.value + "/" + StaminaManager.self.maxValue;
+        }
+    }
 }
